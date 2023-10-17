e16d41a3e54c3ffdf22297e9388f9fc05bb14eb0
pfb03 <pfb03@info03.cshl.edu>
2023-10-17 15:51:18 -0400

    aggiornato col 50

--- pos_or_neg.py
+++ pos_or_neg.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import sys
 value = int(sys.argv[1])
-if value > 0:
+if value > 0: #testo se i valori inseriti siano positivi, negativi, sopra o sotto al 50
    print('positive')
+   if value <50:
+       print('value positive but not enough')
+   else:
+       print('value pisitive and enough')
+
 elif value < 0:
    print('negative')
 else:
