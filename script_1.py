e2b6cba95ee08c85beab06877d4767e2c0b9ca94
pfb03 <pfb03@info03.cshl.edu>
2023-10-17 11:15:25 -0400

    upload with favourite activity

--- script_1.py
+++ script_1.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
-print("I'm colorblind")
+print("I'm colorblind") #testando aggiungo colore preferito e il mio nome
 print("my name is Guglielmo, il pazzo bello")
+activity = 'I love to build sand castles' #creating a variable with my favourtie activity in it
+print(activity)
+
