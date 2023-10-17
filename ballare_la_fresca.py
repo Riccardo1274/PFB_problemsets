a5719855820666ce5d39ec2f1c78389aaf397671
pfb03 <pfb03@info03.cshl.edu>
2023-10-17 16:52:43 -0400

    ballaggio della fresca fancy

--- ballare_la_fresca.py
+++ ballare_la_fresca.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-fresca=int(input('quanta fresca hai in euro? '))
+fresca=int(input('quanta fresca hai in euro?\n'))
 if fresca >= 100000:
-   gaina=input('sei in gaina? si o no')
+   gaina=input('sei in gaina? si o no\n')
    if gaina == 'si':
       print('andiamo a ballarci la fresca')
    else:
       print('vaffanculo Parenzo stai zitto che bevi i vini del supermercato')
 else:
    print('sei stirato, vai a fare un castelletto')
-   prestito=input('quanto vuole di prestito sig. Stirato?' )
+   prestito=input('quanto vuole di prestito sig. Stirato?\n' )
    print('mi ballo ',prestito,'sui cammelli virtuali')
 
