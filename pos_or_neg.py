e5568c011e0de40abd924cbec87ebf54944ccabf
pfb03 <pfb03@info03.cshl.edu>
2023-10-17 16:04:08 -0400

    aggiornando con pari dispari e divisibile per tre

--- pos_or_neg.py
+++ pos_or_neg.py
@@ -5,8 +5,16 @@ if value > 0: #testo se i valori inseriti siano positivi, negativi, sopra o sott
    print('positive')
    if value <50:
        print('value positive but not enough')
+       if value%2 == 0:
+          print('value is also even')
+       else:
+          print('value is not even')
    else:
-       print('value pisitive and enough')
+       print('value positive and enough')
+       if value%3 == 0:
+             print('divisble by 3')
+       else:
+             print('not divisible by 3')
 
 elif value < 0:
    print('negative')
