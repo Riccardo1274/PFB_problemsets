95f2ae37b11a97c226ab36ad9e10cfb58515e356
pfb03 <pfb03@info03.cshl.edu>
2023-10-17 11:55:18 -0400

    mystory

---
+++ lamiastoria.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+#creating all the objects
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+
+#printing the output
+print('I am', name,' my favourite color is ',color,' I like to', activity)
