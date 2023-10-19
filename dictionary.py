af8f1dc85e5bc731dd1a90d627cba5cc9190f065
pfb03 <pfb03@info03.cshl.edu>
2023-10-19 11:02:32 -0400

    finish dictionary

--- dictionary.py
+++ dictionary.py
@@ -3,7 +3,11 @@
 fav={'animal' : 'Freddy' , 'bird' : 'Motacilla flava' , 'activity' : 'ringing'}
 print(fav)
 print(fav['bird'])
+fav['bird']=input("Which is your favourite bird?")
 fav_t='activity'
 print(fav[fav_t])
 fav['name']='lupoalberto'
 print(fav)
+fav['name']='lupolucio'
+for favs in fav:
+  print(favs,'\t', fav[favs])
