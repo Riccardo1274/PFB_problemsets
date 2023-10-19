0a8cf37b06246ddea4015fd2e0b94cd60014d28c
pfb03 <pfb03@info03.cshl.edu>
2023-10-18 20:30:32 -0400

    while_maths.py

---
+++ while_maths.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#creating a script to print all numbers from 1 to 100
+count=0
+while count < 100:
+  count += 1
+  print(count)
+print('finish')
+#counting 1000!
+count1000=0
+fact=1
+while count1000 < 1000:
+ count1000 += 1
+ fact = fact*count1000
+print(fact)
+
+ 
+
+
