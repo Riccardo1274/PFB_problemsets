41789c66d10b3dd783c5203c6888e920527772f6
pfb03 <pfb03@info03.cshl.edu>
2023-10-18 20:48:50 -0400

    adding count even and odds

--- while_maths.py
+++ while_maths.py
@@ -12,7 +12,23 @@ while count1000 < 1000:
  count1000 += 1
  fact = fact*count1000
 print(fact)
-
- 
-
-
+#printing even from a known list
+values = [101, 2, 15, 22, 95, 33, 2, 27, 72, 15, 52]
+for num in values:
+  even=num%2
+  if even==0:
+    print(num)
+#summing the evens with evens and odds with odds
+print(values)
+sum_even=0
+sum_odds=0
+for num in values:
+  print(num)
+  even2=num%2
+  if even2==0:
+    sum_even += num
+  else:
+    sum_odds += num
+print(sum_even)
+print(sum_odds)
+    
