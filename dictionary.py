a27b64455d23ece31aa7af983a4f634d4287e500
pfb03 <pfb03@info03.cshl.edu>
2023-10-18 22:14:05 -0400

    adding a script with basic dictionaries

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#creating a dictionary
+fav={'animal' : 'Freddy' , 'bird' : 'Motacilla flava' , 'activity' : 'ringing'}
+print(fav)
+print(fav['bird'])
+fav_t='activity'
+print(fav[fav_t])
+fav['name']='lupoalberto'
+print(fav)
