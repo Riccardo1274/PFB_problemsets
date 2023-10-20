6798e786b1fa843c982a4b30bd09bbb5cb866ed6
pfb03 <pfb03@info03.cshl.edu>
2023-10-20 17:29:00 -0400

    non perfetto dizionario printato con ordine di lunghezza

--- Apol.py
+++ Apol.py
@@ -2,6 +2,17 @@
 import re
 #finding the sites for restriction enzyme Apol
 fasta = open('apol.fasta', 'r')
+final_string=''
 for line1 in fasta:
    sub = re.sub(r'([AG]AATT[CT])', r'\1^',line1)
-   print(sub)
+   final_string += sub
+final_string=final_string.replace('\n', '')
+print(final_string)
+
+sep=final_string.split('^')
+dic = {}
+for line in sep:
+  dic[len(line)] = line
+for seq in sorted(dic):
+  print(seq , '-->', dic[seq])
+
