48ddd77053a52cff3ada67f3ca9dac25b0a884d6
pfb03 <pfb03@info03.cshl.edu>
2023-10-24 20:48:29 -0400

    created a dicionary with a fasta

---
+++ datastructure.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+
+#counting acgt content in a fasta with a fata structure to store the information
+head = 'to_sub'
+seq_tot = ''
+beginning_fasta =open(sys.argv[1], 'r')
+t_list=['to_sub', 'to_sub']
+fasta_dict = {} #creating a dictionary with the fasta
+for line in beginning_fasta:
+  if line.startswith('>'):  #if i have the header, i just add it but when the for loop finished te seq
+     seq_tot = ''.join(t_list) #faster way to compact a string
+     fasta_dict[head] = seq_tot 
+     seq_tot = ''  
+     head = line.replace('\n', '')
+     t_list = []
+  else:
+     seq = line.replace('\n', '')
+     t_list.append(seq) # creating the list to compact the sequence
+seq_tot = ''.join(t_list)
+fasta_dict[head] = seq_tot #adding the last sequence
+
+print (fasta_dict)
+
+
